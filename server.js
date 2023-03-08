d92f26a627c01d041a76bb63371e2b3de71a9e33
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-03-08 13:03:15 -0500

    ..

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
 const path = require('path');
 const express = require('express');
 // // Import express-session
-// // const session = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({})
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 // // const helpers = require('./utils/helpers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const { User, BlogPost, Comment } = require('./models/index')
-// // Set up sessions
-// // const sess = {
-// //   secret: 'Super secret secret',
-// //   resave: false,
-// //   saveUninitialized: false,
-// // };
+const sess = {
+  secret: process.env.SECRET,
+  // Express session will use cookies by default, but we can specify options for those cookies by adding a cookies property to our session options.
+  cookie: {
+    // maxAge sets the maximum age for the cookie to be valid. Here, the cookie (and session) will expire after one hour. The time should be given in milliseconds.
+    maxAge: 60 * 60 * 1000,
+    // httpOnly tells express-session to only store session cookies when the protocol being used to connect to the server is HTTP.
+    httpOnly: true,
+    // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, and running a server without encryption will result in the cookies not showing up in your developer console.
+    secure: false,
+    // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain out server is hosted from.
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  // Sets up session store
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-// // app.use(session(sess));
+app.use(session(sess));
 
 // // const hbs = exphbs.create({ helpers });
 
