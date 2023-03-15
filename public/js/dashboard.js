b2ac851db6ff29f4df52bdce312ccad140daf1db
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-03-15 16:46:26 -0400

    started styling

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,7 +3,6 @@ const createNewBlogBtn = document.getElementById('createNewBlog');
 const newBlogForm = document.getElementById('newBlogForm');
 const blogList = document.querySelector('.blog-list');
 const updateBlogBtns = document.querySelectorAll('[id=update-id]');
-// const updateBlogForm = document.getElementById('update-blog-form');
 const submitUpdatedBlogBtns = document.querySelectorAll('[id=submit-updated-blog]')
 
 const blogPostSubmit = async (event) => {
@@ -11,8 +10,6 @@ const blogPostSubmit = async (event) => {
   
     const title = document.querySelector('#blogTitleInput').value.trim();
     const contents = document.querySelector('#blogContentInput').value.trim();
-    // const user_id = userData.id;
-    // console.log()
     if (title && contents) {
       const response = await fetch('/api/blogpost', {
         method: 'POST',
@@ -50,48 +47,22 @@ const delButtonHandler = async (event) => {
     }
   };
 
-// const handleUpdateBlogBtn = (event) => {
-//     updateBlogBtn.classList.add('d-none');
-//     // blogList.classList.add('d-none');
-//     updateBlogForm.classList.remove('d-none');
-    
-// }
 
 const getUpdateBlogForm = (id) => {
     return document.getElementById(`update-blog-form-${id}`);
 }
+
 const updateFormHandler = async (event) => {
     event.preventDefault()
     const updateId = event.currentTarget.getAttribute("updateId")
     console.log(event.currentTarget.getAttribute("updateId"))
 
-    console.log('form id?')
     const updateForm = getUpdateBlogForm(updateId)
 
-    console.log(event.currentTarget.parentNode)
    if (event.currentTarget.hasAttribute('updateId')){
     event.currentTarget.classList.add('d-none');
-    // blogList.classList.add('d-none');
     updateForm.classList.remove('d-none');
    }
-    // if (event.target.hasAttribute('update')) {
-    //     const id = event.target.getAttribute('update-id-submit');
-    //     // console.log(id);
-    //     const title = document.getElementById('blogTitleUpdate').value.trim()
-    //     const contents = document.getElementById('blogContentUpdate').value.trim()
-    //     if (title && contents && id) {
-    //     const response = await fetch(`/api/blogpost/${id}`, {
-    //         method: 'PUT',
-    //         body: JSON.stringify({ id, title, contents }),
-    //         headers: { 'Content-Type': 'application/json' },
-    //     });
-    //     console.log(response)
-    //     if (response.ok) {
-    //         document.location.replace('/dashboard');
-    //       } else {
-    //         alert('Failed to Update blog');
-    //       }
-    // }}
 }
 
 const submitFormHandler = async (event) => {
@@ -119,13 +90,10 @@ const submitFormHandler = async (event) => {
 
   
 blogList.addEventListener('click', delButtonHandler);
-// blogList.addEventListener('click', updateFormHandler)
 createNewBlogBtn.addEventListener('click', handleBlogForm);
-// updateBlogBtn.addEventListener('click', handleUpdateBlogBtn);
-// submitUpdatedBlogBtn.addEventListener('click', submitFormHandler)
 blogSubmit.addEventListener('click', blogPostSubmit);
 updateBlogBtns.forEach(el => el.addEventListener('click',
-    updateFormHandler //(el.getAttribute("updateId"))
+    updateFormHandler
 ))
 
 submitUpdatedBlogBtns.forEach(el => el.addEventListener('click',
